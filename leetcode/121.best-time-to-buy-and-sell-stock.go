6ec3e985a678c58b4e030a1c78d1d11acf082795
dkf <dannzor@outlook.com>
2020-04-25 13:56:57 +0100

    [leetcode] add 121

---
+++ leetcode/121.best-time-to-buy-and-sell-stock.go
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode id=121 lang=golang
+ *
+ * [121] Best Time to Buy and Sell Stock
+ *
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+ *
+ * algorithms
+ * Easy (49.35%)
+ * Likes:    4442
+ * Dislikes: 198
+ * Total Accepted:    787.7K
+ * Total Submissions: 1.6M
+ * Testcase Example:  '[7,1,5,3,6,4]'
+ *
+ * Say you have an array for which the i^th element is the price of a given
+ * stock on day i.
+ *
+ * If you were only permitted to complete at most one transaction (i.e., buy
+ * one and sell one share of the stock), design an algorithm to find the
+ * maximum profit.
+ *
+ * Note that you cannot sell a stock before you buy one.
+ *
+ * Example 1:
+ *
+ *
+ * Input: [7,1,5,3,6,4]
+ * Output: 5
+ * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit
+ * = 6-1 = 5.
+ * Not 7-1 = 6, as selling price needs to be larger than buying price.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ *
+ *
+ */
+
+// @lc code=start
+func maxProfit(prices []int) int {
+	var maxProfit int
+	var lowestBuy int
+	for i, newPrice := range prices {
+		if i == 0 {
+			lowestBuy = newPrice
+		}
+
+		if newPrice < lowestBuy {
+			lowestBuy = newPrice
+		}
+
+		if newPrice-lowestBuy > maxProfit {
+			maxProfit = newPrice - lowestBuy
+		}
+	}
+
+	return maxProfit
+}
+
+// @lc code=end
