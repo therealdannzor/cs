1cb3486df37816b6ee664e8ac63a4a90777f286f
dkf <dannzor@outlook.com>
2020-05-04 00:13:15 +0100

    [leetcode] add 189 (lc submit go bug)

---
+++ leetcode/189.rotate-array.go
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=189 lang=golang
+ *
+ * [189] Rotate Array
+ *
+ * https://leetcode.com/problems/rotate-array/description/
+ *
+ * algorithms
+ * Easy (33.61%)
+ * Likes:    2403
+ * Dislikes: 771
+ * Total Accepted:    446.1K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[1,2,3,4,5,6,7]\n3'
+ *
+ * Given an array, rotate the array to the right by k steps, where k is
+ * non-negative.
+ *
+ * Example 1:
+ *
+ *
+ * Input: [1,2,3,4,5,6,7] and k = 3
+ * Output: [5,6,7,1,2,3,4]
+ * Explanation:
+ * rotate 1 steps to the right: [7,1,2,3,4,5,6]
+ * rotate 2 steps to the right: [6,7,1,2,3,4,5]
+ * rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: [-1,-100,3,99] and k = 2
+ * Output: [3,99,-1,-100]
+ * Explanation:
+ * rotate 1 steps to the right: [99,-1,-100,3]
+ * rotate 2 steps to the right: [3,99,-1,-100]
+ *
+ *
+ * Note:
+ *
+ *
+ * Try to come up as many solutions as you can, there are at least 3 different
+ * ways to solve this problem.
+ * Could you do it in-place with O(1) extra space?
+ *
+ */
+
+// @lc code=start
+func rotate(nums []int, k int) {
+	n := len(nums)
+
+	for k > 0 {
+		last := []int{nums[n-1]}
+		nums = append(last, nums...)
+		nums = nums[:n]
+		k--
+	}
+}
+
+// @lc code=end
